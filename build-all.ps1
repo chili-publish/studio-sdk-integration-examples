4ef752ffbef608c0197aaa3363b69d9e19da29a2
Pieter Van Parys <pieter.vanparys@gmail.com>
2022-11-10 16:24:36 +0100

    Update build script

--- build-all.ps1
+++ build-all.ps1
@@ -13,6 +13,19 @@ finally {
 
 try {
     # set working directory
+    Push-Location -Path "ts-connector-example"
+    
+    # execute    
+    yarn
+    yarn upgrade @chili-publish/editor-sdk --latest
+    yarn build
+}
+finally {
+    Pop-Location
+}
+
+try {
+    # set working directory
     Push-Location -Path "ts-sketchfab-example"
     
     # execute    
