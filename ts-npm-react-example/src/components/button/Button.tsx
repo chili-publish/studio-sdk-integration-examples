e3fcd341f19554203d9e837e79d1429ba77917d4
brapoprod <broels@chili-publish.com>
2022-11-15 10:34:56 +0100

    move to correct folder

---
+++ ts-npm-react-example/src/components/button/Button.tsx
@@ -0,0 +1,17 @@
+import React, { FC, ReactNode } from 'react';
+
+type ButtonProps = {
+    onClick: () => void;
+    isDisabled?: boolean;
+    children: ReactNode
+}
+
+const Button: FC<ButtonProps> = (props) => {
+    const {onClick, isDisabled = false, children} = props;
+
+    return (
+        <button onClick={onClick} disabled={isDisabled} >
+            {children}
+        </button>
+    )
+}
